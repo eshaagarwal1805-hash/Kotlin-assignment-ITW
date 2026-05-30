727ae13ee55262186ad898aa54c5b74439f7f214
Esha Agarwal <eshaagarwal1805@gmail.com>
2026-05-30 15:14:49 +0530

    Refactor list filtering with original list output

--- q13.kt
+++ q13.kt
@@ -1,3 +1,5 @@
 fun main() {
-    println(listOf(1, 2, 3, 4, 5).filter { it % 2 == 0 })
+    val list1 = listOf(1,2,3,4,5)
+    println("the original list: " +list1)
+    println("the filtered list: " +list1.filter { it % 2 == 0 })
 }
